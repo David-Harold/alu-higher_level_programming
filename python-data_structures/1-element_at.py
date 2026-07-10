336ea8e9f561aca8ddb1071803cdfebd4ed7b336
David-Harold <ke.d08@yahoo.com>
2026-07-10 14:48:26 -0700

    Update:Fixing issues in 1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Module that retrieves an element from a list like in C."""
- 
- 
+
+
 def element_at(my_list, idx):
-    """Return the element at idx, or None if idx is negative or out of range."""
+    """Return the element at idx, or None if idx is negative
+    or out of range."""
     if idx < 0 or idx >= len(my_list):
         return None
     return my_list[idx]
