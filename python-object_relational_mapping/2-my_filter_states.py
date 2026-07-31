abd003a3aba3f0be5ebf05bafb6df2af064cf9a4
David-Harold <ke.d08@yahoo.com>
2026-07-31 11:16:17 -0700

    python-object_relational_mapping: force BINARY comparison for case-sensitive filters

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -14,8 +14,9 @@ if __name__ == "__main__":
         host="localhost", port=3306,
         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
-        sys.argv[4])
+    query = (
+        "SELECT * FROM states WHERE name = BINARY '{}' "
+        "ORDER BY id ASC").format(sys.argv[4])
     cur.execute(query)
     for row in cur.fetchall():
         print(row)
--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         "SELECT cities.name "
         "FROM cities "
         "JOIN states ON cities.state_id = states.id "
-        "WHERE states.name = %s "
+        "WHERE BINARY states.name = %s "
         "ORDER BY cities.id ASC",
         (sys.argv[4],))
     cities = [row[0] for row in cur.fetchall()]
