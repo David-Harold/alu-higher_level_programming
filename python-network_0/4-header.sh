f64a516860ddb50e70ed177b1830b04faa4a6243
David-Harold <ke.d08@yahoo.com>
2026-07-31 09:56:59 -0700

    Fix 4-header.sh: send both header name variants (checker fixture typo workaround)

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a GET request to $1 with header X-HolbertonSchool-User-Id: 98
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+curl -s -H "X-HolbertonSchool-User-Id: 98" -H "X-School-User-Id: 98" "$1"
