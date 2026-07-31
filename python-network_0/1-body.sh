ac89c988160ecce0f60a2ec2785bf40e8f415d0d
David-Harold <ke.d08@yahoo.com>
2026-07-31 09:33:08 -0700

    Fix 1-body.sh: 3 lines, follow redirects

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# displays the body of a GET response to $1 only if status code is 200
-curl -s -o /tmp/body_$$ -w "%{http_code}" "$1" | grep -q 200 && cat /tmp/body_$$
+# displays the body of a GET response to $1 (following redirects) only if the final status code is 200
+curl -s -L -o /tmp/body_$$ -w "%{http_code}" "$1" | grep -q 200 && cat /tmp/body_$$
